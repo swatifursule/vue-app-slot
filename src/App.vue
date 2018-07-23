018b743a53eb76e5a8df7dcc68b5192a0a8dd22f
Swati Fursule <swati@silvertrak.com.au>
2018-07-23 16:24:02 +1000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1 @click="onClick">
+    {{message}}
+  </h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+import Vue from 'vue';
+import Component from 'vue-class-component';
+export default Component({})(
+  class App extends Vue {
+    message = 'Swati';
+    onClick() {
+      this.message = 'Hello';
+    }
   }
-}
+);
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import 'tailwindcss/dist/tailwind.css';
 import Vue from 'vue'
 import App from './App.vue'
 
